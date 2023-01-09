2170bea17ae45efc23b89c26848536ca17e8d50d
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2023-01-09 21:05:57 +0530

    Create AlertTarget.java

---
+++ src/main/java/TypewiseAlert/AlertTarget.java
@@ -0,0 +1,22 @@
+package TypewiseAlert;
+
+public enum AlertTarget {
+	TO_CONTROLLER, TO_EMAIL;
+	
+	public static boolean raiseAlert(BreachType breachType) {
+		return breachType!=BreachType.NORMAL;
+	}
+
+	public static void sendToController(BreachType breachType) {
+		int header = 0xfeed;
+		System.out.printf("%i : %i\n", header, breachType);
+	}
+
+	public static void sendToEmail(BreachType breachType) {
+		String recepient = "a.b@c.com";
+		if(raiseAlert(breachType))
+			System.out.printf("To: %s\n", recepient);
+			System.out.println(breachType.getMessage());
+		}
+	}
+
