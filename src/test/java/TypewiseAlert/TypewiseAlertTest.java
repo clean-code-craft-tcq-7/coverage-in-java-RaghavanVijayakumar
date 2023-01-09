6c37cae0d55a0b00da55b887803ab011591197f9
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2023-01-09 21:08:21 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,6 +1,10 @@
 package TypewiseAlert;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.Test;
 
@@ -9,7 +13,31 @@ public class TypewiseAlertTest
     @Test
     public void infersBreachAsPerLimits()
     {
-      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+      assertTrue(BreachAnalyzer.inferBreach(12, 20, 30) == BreachType.TOO_LOW);
+      assertTrue(BreachAnalyzer.inferBreach(35, 10, 35) == BreachType.NORMAL);
+      assertTrue(BreachAnalyzer.inferBreach(45, 10, 40) == BreachType.TOO_HIGH);
+      assertTrue(BreachAnalyzer.inferBreach(10, 10, 30) == BreachType.NORMAL);
+      assertFalse(BreachAnalyzer.inferBreach(45, 10, 30) == BreachType.NORMAL);
+      assertFalse(BreachAnalyzer.inferBreach(12, 20, 30) == BreachType.NORMAL);
+    }
+    
+    @Test
+    public void assertCoolingTypelimit()
+    {
+      assertEquals(CoolingType.PASSIVE_COOLING.upper_limit,35);
+      assertEquals(CoolingType.PASSIVE_COOLING.lower_limit,0);
+      assertEquals(CoolingType.HI_ACTIVE_COOLING.lower_limit,0);
+      assertEquals(CoolingType.HI_ACTIVE_COOLING.upper_limit,45);
+      assertEquals(CoolingType.MED_ACTIVE_COOLING.lower_limit,0);
+      assertEquals(CoolingType.MED_ACTIVE_COOLING.upper_limit,40);
+      assertNotEquals(CoolingType.HI_ACTIVE_COOLING.upper_limit,50);
+    }
+    
+    @Test
+    public void assertBreachTypemessage()
+    {
+    assertTrue(BreachType.NORMAL.message=="Temperature is normal");
+    assertTrue(BreachType.TOO_HIGH.message=="Hi, the temperature is too high");
+    assertTrue(BreachType.TOO_LOW.message=="Hi, the temperature is too low");
     }
 }
